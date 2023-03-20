fda0a35684846a7c245905a3a577b8b44eafb226
Ivan <fenomen_ny@mail.ru>
2023-03-20 20:29:05 +0300

    add 83

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/83.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: 'Rex',
+    legs: 4,
+    tails: 1,
+    friends: ['Ivan', 'Kristina', 'Linda', 'Jeck']
+    // Only change code above this line
+};
