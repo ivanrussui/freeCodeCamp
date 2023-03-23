1a01f32919f5b47ee351fcb0c782318c5ba5f6d2
Ivan <fenomen_ny@mail.ru>
2023-03-23 23:49:40 +0300

    add 92

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Detsl",
+        "title": "Kto Ti?",
+        "release_year": 2000,
+        "formats": [
+            "CD",
+            "Cassette",
+            "MP3"
+        ]
+    }
+];
